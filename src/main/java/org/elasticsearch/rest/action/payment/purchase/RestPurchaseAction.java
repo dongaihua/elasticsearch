e84a04efbd7677a5ee42d478d9e82f9b8708df74
jackiedong168 <jackiedong168@gmail.com>
2014-02-09 19:50:39 +0800

    use thread to handle payment

--- src/main/java/org/elasticsearch/rest/action/payment/purchase/RestPurchaseAction.java
+++ src/main/java/org/elasticsearch/rest/action/payment/purchase/RestPurchaseAction.java
@@ -1,6 +1,7 @@
 package org.elasticsearch.rest.action.payment.purchase;
 
 import org.elasticsearch.ElasticSearchException;
+import org.elasticsearch.action.PaymentListener;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.inject.Inject;
@@ -10,6 +11,7 @@ import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.payment.Gateway;
 import org.elasticsearch.rest.*;
+import org.elasticsearch.threadpool.ThreadPool;
 
 import java.io.IOException;
 import java.util.Map;
@@ -22,38 +24,73 @@ import static org.elasticsearch.rest.action.support.RestXContentBuilder.restCont
  * Created by jackiedong168 on 14-1-20.
  */
 public class RestPurchaseAction extends BaseRestHandler {
+    private final ThreadPool threadPool;
 
     @Inject
-    public RestPurchaseAction(Settings settings, Client client, RestController controller) {
+    public RestPurchaseAction(Settings settings, Client client, RestController controller, ThreadPool threadPool) {
         super(settings, client);
+        this.threadPool = threadPool;
         controller.registerHandler(POST, "/v1/purchases", this);
     }
 
     @Override
     public void handleRequest(final RestRequest request, final RestChannel channel) {
         Map<String, Object> state = parsePurchaseRequest(request);
-        try {
-            Map newState = Gateway.purchase(state);
-            XContentBuilder builder = null;
-            builder = restContentBuilder(request);
-            builder.startObject()
-                    .field("projectName", "Test")
-                    .field("projectVersion", "1.0")
-                    .field("logType", "Apache")
-                    .field("logSource", "access.log")
-                    .field("logLevel", "1")
-                    .field("logTime", "2012-1-2T12:6:6")
-                    .field("host", "127.0.0.1")
-                    .field("body", "GET /test/nelo/")
-                    .endObject();
-            channel.sendResponse(new XContentRestResponse(request, OK, builder));
+        purchase(state, new PaymentListener<Map>() {
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            @Override
+            public void onResponse(Map state) {
+                try {
+                    XContentBuilder builder = null;
+                    builder = restContentBuilder(request);
+                    builder.startObject()
+                            .field("projectName", "Test2")
+                            .field("projectVersion", "1.0")
+                            .field("logType", "Apache")
+                            .field("logSource", "access.log")
+                            .field("logLevel", "1")
+                            .field("logTime", "2012-1-2T12:6:6")
+                            .field("host", "127.0.0.1")
+                            .field("body", "GET /test/nelo/")
+                            .endObject();
+                    channel.sendResponse(new XContentRestResponse(request, OK, builder));
 
+                } catch (Exception e) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("failed to execute search (building response)", e);
+                    }
+                    onFailure(e);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                try {
+                    channel.sendResponse(new XContentThrowableRestResponse(request, e));
+                } catch (IOException e1) {
+                    logger.error("Failed to send failure response", e1);
+                }
+            }
+        });
     }
 
+
+    private void purchase(final Map<String, Object> state, final PaymentListener<Map> listener) {
+        threadPool.generic().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map newState = Gateway.purchase(state);
+                    listener.onResponse(newState);
+                } catch (Exception e) {
+                    listener.onFailure(e);
+                }
+            }
+        });
+
+
+        }
 
     private Map parsePurchaseRequest(RestRequest request) {
         if (request.hasContent()) {
@@ -70,25 +107,14 @@ public class RestPurchaseAction extends BaseRestHandler {
         return null;
     }
 
-    /*
-    private Map pareseReqeustBody(BytesReference content) {
-        Map<String, Object> map = parseBody(content);
-
-        String [] fields = new String[]{
-                "user-id", "order-id", "order-number", "order-amount", "description",
-                "payment-id", "payment-method-id", "payment-amount"};
-        for(String field : fields) {
-            map.put(field, request.param(field));
-        }
-
-        String creditcard = request.param("creditcard");
-
+    private Map<String, Object> handleRequest(Map<String, ?> state) throws Exception {
+        return (Map<String, Object>) state;
     }
-*/
 
     private Map<String, Object> parseBody(BytesReference content) throws IOException {
         return XContentFactory.xContent(XContentType.JSON).createParser(content).mapAndClose();
     }
+
 
 
 }
